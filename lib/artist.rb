718be42927534f7d024ce41f16ef2acda362fa78
Lupita <lupitarivera8899@gmail.com>
2021-02-17 21:27:33 -0600

    commit tree 3f6afab920698f8ccdef1b4fd1a0c30522a47046

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,29 @@
 
+class Artist
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+      @name = name
+      
+      @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        # binding.pry
+        Song.all.select {|song| song.artist == self} 
+        #returns all songs associated with this Artist
+    end
+
+    def new_song(name,genre)
+        Song.new(name,self,genre)
+        #given a name and genre, creates a new song associated with that artist 
+    end
+
+    def genres
+        songs.collect {|song| song.genre}
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
 
+class Patient
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+      @name = name
+      
+      @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date,doctor)
+        Appointment.new(date,self,doctor)
+    end
+
+    def appointments
+        Appointment.all.select {|appointment| appointment.patient == self}
+        #returns all appointments associated with this Patient
+    end
+
+    def doctors
+        appointments.collect {|appointment| appointment.doctor}
+        #has many doctors through appointments
+    end
+
+end
